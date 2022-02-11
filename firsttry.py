7bee19f5e9c40a899f50f4e89ce7de8013e54e9c
sixianj2 <71730024+sixianj2@users.noreply.github.com>
2022-02-10 19:53:19 -0800

    Add files via upload

--- firsttry.py
+++ firsttry.py
@@ -7,6 +7,27 @@ import asyncio
 power_val = 50
 key = 'status'
 print("If you want to quit.Please press q")
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+def readkey(getchar_fn=None):
+    getchar = getchar_fn or readchar
+    c1 = getchar()
+    if ord(c1) != 0x1b:
+        return c1
+    c2 = getchar()
+    if ord(c2) != 0x5b:
+        return c1
+    c3 = getchar()
+    return chr(0x10 + ord(c3) - 65)
+
 def Keyborad_control():
     while True:
         global power_val
