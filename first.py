54d9279bfea06f17f199718364fdf1af17345b49
sixianj2 <71730024+sixianj2@users.noreply.github.com>
2022-02-10 23:05:34 -0600

    Add files via upload

--- first.py
+++ first.py
@@ -7,7 +7,7 @@ import time
 from picar_4wd.servo import Servo
 from picar_4wd.pwm import PWM
 from picar_4wd.pin import Pin
-
+from picar_4wd.ultrasonic import Ultrasonic
 power_val = 50
 key = 'status'
 print("If you want to quit.Please press q")
@@ -36,24 +36,30 @@ def Keyborad_control():
     while True:
         global power_val
         key=readkey()
-        if key=='6':
-            if power_val <=90:
-                power_val += 10
-                print("power_val:",power_val)
-        elif key=='4':
-            if power_val >=10:
-                power_val -= 10
-                print("power_val:",power_val)
-        if key=='w':
+        if key == 'w':
             fc.forward(power_val)
-        elif key=='a':
-            fc.turn_left(power_val)
-        elif key=='s':
-            fc.backward(power_val)
-        elif key=='d':
-            fc.turn_right(power_val)
-        else:
-            fc.stop()
+            distant1 = Ultrasonic()
+            if distant1 <= 10:
+                fc.stop()
+
+        # if key=='6':
+        #     if power_val <=90:
+        #         power_val += 10
+        #         print("power_val:",power_val)
+        # elif key=='4':
+        #     if power_val >=10:
+        #         power_val -= 10
+        #         print("power_val:",power_val)
+        # if key=='w':
+        #     fc.forward(power_val)
+        # elif key=='a':
+        #     fc.turn_left(power_val)
+        # elif key=='s':
+        #     fc.backward(power_val)
+        # elif key=='d':
+        #     fc.turn_right(power_val)
+        # else:
+        #     fc.stop()
         if key=='q':
             print("quit")
             break
